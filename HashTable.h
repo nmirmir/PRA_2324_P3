4ef76ce76af7c3889ec9223c3305b4b09085509e
Nicolás Miró Mira <nicolasmiromira@gmail.com>
2023-12-30 09:29:08 +0100

    He afegit el makefile actualitzat i la Hastable està casi acabada

--- HashTable.h
+++ HashTable.h
@@ -7,7 +7,7 @@
 #include "Dict.h"
 #include "TableEntry.h"
 
-#include "../PRA_2324_P1/ListArray.h" // actualizar para cuando acabe el ListLinked
+#include "../PRA_2324_P1/ListLinked.h" 
 template <typename V>
 class HashTable: public Dict<V> {
 
@@ -19,27 +19,69 @@ class HashTable: public Dict<V> {
                         int max;
 
                         //Tabla de cubetas, representada por un array de punteros a listas enlazadas (tipo ListLinked<T>) que almacenan pares clave→valor (tipo TableEntry<V>).
-                        ListArray<TableEntry<V>>* table; // Actualizar para cuando acabe el ListLinked
+                        ListLinked<TableEntry<V>>* table; 
                             
 
             public:
+                        //Inserta el par key->value en el diccionario. Lanza una excepción std::runtime_error si key ya existe en el diccionario.
+                        void insert(std::string key, V value){
+                                int pos = h(key);
+                                if(table[pos].search(key) == -1){
+                                        TableEntry<V> nodo(key, value);
+                                        table[pos].prepend(nodo);
+                                        n++;
+                                }else{
+                                        throw ("error de insercción");
+                                }
+                        }
+
+                        //Busca el valor correspondiente a key. Devuelve el valor value asociado si key está en el diccionario. Si no se encuentra, lanza una excepción std::runtime_error.
+                        V search(std::string key){
+                                int pos = h(key);
+                                int p = table[pos].search(key);
+                                if(p == -1){
+                                        throw std::runtime_error("la clave no se encuentra");
+                                }
+                                TableEntry<V> nodo = table[pos].get(p);
+                                return nodo.value;
+                        }
+
+                        //Elimina el par key->value si se encuentra en el diccionario. Si no se encuentra, lanza una excepción std::runtime_error.
+                        V remove(std::string key){
+                                int pos = h(key);
+                                int p = table[pos].search(key);
+                                if(p == -1){
+                                        throw std::runtime_error("la clave no se encuentra");
+                                }
+                                n--;
+                                TableEntry<V> nodo = table[pos].remove(p);
+                                return nodo.value;
+                        }
+
+                        //Devuelve el número de elementos que tiene el Diccionario.
+                        int entries(){
+                                return n;
+                        }
+
+
+
                         //Función hash que devuelve la posición (cubeta) en la tabla hash de key.  Se calculará como el resto de la divisón entre la suma de los valores ASCII numéricos de los caracteres de la clave y el tamaño de la tabla hash (ver nota más abajo).
                         int h(std::string key){
                                 int suma = 0;
                                 int i = 0;
-                                while(key.at(i) != '\0'){
+                                while(key.at(i)){
                                         suma += int(key.at(i));
                                         i++;
                                 }
-                                int pos;
-                                return pos = suma % n;
+                                return suma % max;
                         }
                         
                         //Método constructor. Reservará memoria dinámica para crear una tabla table de tamaño size, e inicializará los atributos n y max de la clase.
+                        
                         HashTable(int size){
                                 int n = 0;
-                                int max = size -1;
-                                table = new V[size];
+                                int max = size;
+                                table = new ListLinked<TableEntry<V>>[size];
                         }
 
                         //Método destructor. Se encargará de liberar la memoria dinámica reservada al crear la tabla table.
@@ -49,18 +91,33 @@ class HashTable: public Dict<V> {
 
                         //Devuelve el número total de cubetas de la tabla.
                         int capacity(){
-                                return n;
+                                return max;
                         }
 
                         //Sobrecarga global del operador << para imprimir el contenido de la tabla hash por pantalla. Recuerda incluir la cabecera <ostream> en el .h.
                         friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th){
 
-                                for(int i = 0)
+                                out << "HashTable [ entries: "<< th.n << "capacity: "<< th.max << "]"<<std::endl;
+
+                                for(int i = 0; i < th.max ; i++){
+                                        out << "== Cubeta " << i << "==" << "List => [" <<std::endl;
+                                        for(int j = 0; j < th.table[i].size(); j++){
+
+                                                out << th.table[i].get(j) <<std::endl; 
+
+                                        }
+
+                                        out << "]";
+                                }
+
+                                return out;
 
                         }
 
                         //Sobrecarga del operador []. Devuelve el valor correspondiente a key. Si no existe, lanza la excepción std::runtime_error.
-                        V operator[](std::string key);
+                        V operator[](std::string key){
+                                return search(key);
+                        }
 
 };
 
--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 bin/testTableEntry: tableEntry.h testTableEntry.cpp
 	mkdir -p bin
 	g++ -o bin/testTableEntry testTableEntry.cpp tableEntry.h
+bin/testHashTable: HashTable.h testHashTable.cpp
+	mkdir -p bin
+	g++ -o bin/testHashTable testHashTable.cpp tableEntry.h HashTable.h Dict.h
 clean:
-	rm -r *gch bin *o
+	rm -r *.gch bin *.o
