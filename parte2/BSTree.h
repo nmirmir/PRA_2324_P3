6e829074404825ab5b7343ab77f09ac2f64d8edd
Nicolás Miró Mira <nicolasmiromira@gmail.com>
2024-01-04 11:51:45 +0100

    BSTree con los métodos implementados pero todavía con errores

--- parte2/BSTree.h
+++ parte2/BSTree.h
@@ -37,8 +37,8 @@ class BSTree {
                     //Notar que deberá devolver el elemento contenido por el nodo devuelto por el método privado.
                     T search(T e) const{
                             BSNode<T> nodo_aux; 
-                            nodo_aux = search(root,e) const; // r -> nodo raíz
-                            return nodo_aux.elem;
+                            nodo_aux = search(root,e); // r -> nodo raíz
+                            return nodo_aux->elem;
                     }
 
             private:
@@ -47,11 +47,11 @@ class BSTree {
                     //Busca y devuelve el elemento e de tipo T si se encuentra en el (sub-)árbol cuya raíz es n, de lo contrario lanza una std::runtime_error.
                     BSNode<T>* search(BSNode<T>* n, T e) const{
                         if(n == nullptr){
-                                throw runtime_error("árbol vacío");        
-                        }else if(n.elem > e){
-                                return search(n.left,e);
-                        }else if(n.elem < e){
-                                return search(n.right,e);
+                                throw std::runtime_error("árbol vacío");        
+                        }else if(n->elem > e){
+                                return search(n->left,e);
+                        }else if(n->elem < e){
+                                return search(n->right,e);
                         }else{
                                 return n; // se hace porque ya se ha encontrado el elemento
                         }
@@ -74,12 +74,12 @@ class BSTree {
                     BSNode<T>* insert(BSNode<T>* n, T e){
                         if(n == nullptr){
                                 return BSNode<T> nodo;
-                        }else if(n.elem == e){
-                                throw runtime_error("el elemento ya existe");
-                        }else if(n.elem > e){
-                                n.left = return insert(n.left,e);               
-                        }else(n.elem < e){
-                                n.right = return insert(n.right,e);
+                        }else if(n->elem == e){
+                                throw std::runtime_error("el elemento ya existe");
+                        }else if(n->elem > e){
+                                return n->left = insert(n->left,e);               
+                        }else{
+                                return n->right = insert(n->right,e);
                         }
                         return n;
                     }
@@ -89,24 +89,100 @@ class BSTree {
                    friend std::ostream& operator<<(std::ostream &out, const BSTree<T> &bst){
                         out << print_inorder(&out, &bst) << std::endl;
                         return out;
-                   }             
+                   }
+
+           private:
                    //Recorrido inorden o simétrico del (sub-)árbol cuya raíz es n para mostrar a través de out los elementos ordenados de menor a mayor. Ver pseudocódigo abajo. 
                    void print_inorder(std::ostream &out, BSNode<T>* n) const{
                         if(n != nullptr){
-                                if(n.left != nullptr && (n.right != nullptr || n.right == nullptr)){
-                                        n.left = print_inorden(&out, n.left);
-                                        out << "nodo" << n.left << std::endl;
-                                }else if(n.right != nullptr && (n.left != nullptr || n.left == nullptr)){
-                                        out << "nodo" << print_inorden(&out, n.right); 
+                                if(n->left != nullptr ){
+                                        n->left = print_inorder(&out, n->left);
+                                        out << "nodo" << n->left << std::endl;
+                                }else if(n->right != nullptr ){
+                                        n->right = print_inorder(&out, n->right)
+                                        out << "nodo" << n->right; 
                                 }
-                                for(int i = 0; i < n.size(); i++){
-                                        if(n.left != nullptr && (n.right != nullptr || n.right == nullptr)){
-                                                out << print_inorden(&out, n.left); 
-                                        }else if(n.right != nullptr && (n.left != nullptr || n.left == nullptr)){
-                                                out << print_inorden(&out, n.right); 
+                                out << n << std::endl;
+
+                                for(int i = 0; i < n->size(); i++){
+                                        print_inorder(&out, n);
+                                }
+                        }
+                   }
+           public:
+                   //Elimina el elemento e de tipo T del ABB. Actúa como método lanzadera del método privado recursivo remove(BSNode<T>* n, T e).
+                   void remove(T e){
+                        BSNode<T> nodo;
+                        nodo.elem = remove(nodo, e);
+                   }
+           private:
+                   //Método recursivo para la eliminación de elementos. 
+                   //Elimina el elemento e del (sub-)árbol cuya raíz es n. 
+                   //Devuelve el nodo que encabeza dicho (sub-)árbol modificado. 
+                   //En caso de eliminar un nodo con dos sucesores (izquierdo y derecho), 
+                   //aplicará la politica de reemplazo por el elemento máximo del subárbol izquierdo.
+                   //Para ello, se apoyará en los métodos privados auxiliares max() y remove_max(). 
+                   //Si el elemento e no existe, lanza un std::runtime_error. Ver pseudocódigo abajo. 
+                   BSNode<T>* remove(BSNode<T>* n, T e){
+                        if(n == nullptr){
+                                throw std::runtime_error("no hay elementos en el ABB");
+                        // Ahora vamos a mirar hacia qué rama vamos a ir, si a la derecha o a la izquierda        
+                        }else if(n->elem < e){
+                                n->right = remove(n->right, e); // rama derecha
+                        }else if(n->elem > e){
+                                n->left = remove(n->left, e); // rama izquierda
+                        }else{
+                                //Una vez sabemos a qué rama nos dirigimos, valoramos el nº de descendientes
+                                if(n->left != nullptr && n->right != nullptr){
+                                        n->elem = max(n->left);
+                                        n->left = remove_max(n->left);
+                                }else{
+                                // Este caso es para ver si tiene 1 o 0 descendientes
+                                        if(n->left == nullptr){
+                                                n = (n->left,e);
+                                        }else if(n->left == nullptr){
+                                                n = (n->right,e);
                                         }
                                 }
 
+                        }
+                        return n;
+                   }
+
+                   //Método recursivo que devuelve el elemento de máximo valor contenido en el (sub-)árbol cuya raíz es n. 
+                   T max(BSNode<T>* n) const{
+                        if(n == nullptr){
+                                throw std::runtime_error("no hay elementos en el ABB");
+                        }else if(n->right != nullptr){
+                                return max(n->right);
+                        }else{
+                                return n->elem;
+                        }
+                   }
+
+                   //Método recursivo que elimina el elemento de máximo valor contenido en el (sub-)árbol cuya raíz es n. 
+                   BSNode<T>* remove_max(BSNode<T>* n){
+                        // Primero comprobamos si el nodo tiene descendientes a la derecha, para ver si él es el máximo o no, en caso de que no haber, se devuelve el hijo izquierdo para eliminar el máximo.  
+                        if(n->right == nullptr){
+                                return n->left;
+                        }else{
+                        // Todavía hay nodos mayores, así que se llamará de forma recursiva hasta llegar al caso base, en el que se encuentra el máximo.
+                                n->right = remove_max(n->right);
+                                return n;
+                        }
+                   }
+           public:
+                   //Método destructor. Delega en el método privado recursivo delete_cascade().
+                   ~BSTree(){
+                        BSNode<T> nodo;   
+                        delete_cascade(nodo);
+                   }
+
+           private:
+                   //Método recursivo para la liberación de la memoria dinámica ocupada por los objetos de tipo BSNode<T> que conforman el (sub)-árbol cuya raíz es n.
+                   void delete_cascade(BSNode<T>* n){
+                        if(n != nullptr){
+                                remove(n,e);
                         }
                    }
 
